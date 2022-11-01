3c2af53fc701d015a3641ce929d47aa6ea5fb7ac
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-01 17:36:03 +0100

    inventory implementation

--- Inventory.py
+++ Inventory.py
@@ -1,50 +1,68 @@
+import pygame
+from pygame.surface import Surface
+
 from Item import Item
 from Slot import Slot
+from game.CameraSpriteGroup import CameraSpriteGroup
 
 
-class Inventory:
-    def __init__(self, size: int):
-        self._inventoryList: list[Slot] = [Slot()] * size
-        self._size: int = size
+class Inventory(pygame.sprite.Sprite):
+    def __init__(self,
+                 visibleSprites: CameraSpriteGroup,
+                 inventoryHeight: int,
+                 inventoryWidth: int,
+                 selectedItem: Item | None = None):
+
+        super().__init__()
+        self._inventoryHeight = inventoryHeight
+        self._inventoryWidth = inventoryWidth
+        self._selectedItem = selectedItem
+        self._inventoryList: list[Slot] = [Slot()] * inventoryWidth * inventoryHeight
+        self._isOpened: bool = False
+
+        self._visibleSprites = visibleSprites
+        self._image: Surface = pygame.image.load("path_to_image")
+        self._rect = self.image.get_rect()
+        self._center = pygame.math.Vector2()
 
     @property
-    def inventoryList(self) -> list[Slot]:
-        return self._inventoryList
-
-    @inventoryList.setter
-    def inventoryList(self, newInventory: list[Slot]) -> None:
-        self.inventoryList = newInventory
+    def selectedItem(self) -> Item | None:
+        return self._selectedItem
 
     @property
-    def size(self) -> int:
-        return self._size
+    def isOpened(self) -> bool:
+        return self._isOpened
 
-    @size.setter
-    def size(self, newSize: int) -> None:
-        if newSize < self.size:
-            self.inventoryList = self.inventoryList[:self.size - newSize]
-            self.size = newSize
-        else:
-            self.inventoryList += [Slot()] * (newSize - self.size)
-            self.size = newSize
+    def open(self) -> None:
+        self._visibleSprites.add(self)
+        self._visibleSprites.add(*self._inventoryList)
+        self._isOpened = True
+
+    def close(self) -> None:
+        self._visibleSprites.remove(self)
+        self._visibleSprites.remove(*self._inventoryList)
+        self._isOpened = False
 
     def addItem(self, item: Item) -> None:
-        first: Slot | None = next(filter(lambda slot: (slot.isEmpty()), self.inventoryList), None)
-        if first is None:
-            raise ValueError("The inventroy is full")
-        else:
-            indexOfSlot: int = self.inventoryList.index(first)
-            self.inventoryList[indexOfSlot].item = item
+        first: Slot | None = next(filter(lambda slot: (slot.isEmpty()), self._inventoryList), None)
+        if first is None and self._selectedItem is not None:
+            pos = pygame.mouse.get_pos()
+            self._selectedItem.drop(*pos)
+            self._selectedItem = item
+            return
+        if first is None and self._selectedItem is None:
+            self._selectedItem = item
+            return
+        indexOfSlot: int = self._inventoryList.index(first)
+        self._inventoryList[indexOfSlot].item = item
+        return
 
-    def deleteItem(self, index: int) -> None:
-        if index >= self.size:
-            raise ValueError("Index out of range")
-        self.inventoryList[index].dropItem()
-
-    def findAndDeleteItem(self, item: Item) -> None:
-        first: Slot | None = next(filter(lambda slot: (slot.item is item), self.inventoryList), None)
-        if first is None:
-            raise ValueError("Can not find the item")
-        else:
-            indexOfSlot: int = self.inventoryList.index(first)
-            self.inventoryList[indexOfSlot].dropItem()
+    def update(self) -> None:
+        pos = pygame.mouse.get_pos()
+        if self._selectedItem is not None:
+            self._selectedItem.rect.center = pos
+            return
+        hooveredSlots = [s for s in self._inventoryList if s.rect.collidepoint(pos)]
+        for slot in hooveredSlots:
+            slot.conditionalUpdate()
+        return

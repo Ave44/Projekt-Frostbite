6fb46ad8f0125ef3c5f71300a4f47266917d5f1c
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-02 21:15:25 +0100

    refactoring

--- game/Inventory.py
+++ game/Inventory.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.math import Vector2
 from pygame.surface import Surface
 
 from Item import Item
@@ -11,19 +12,23 @@ class Inventory(pygame.sprite.Sprite):
                  visibleSprites: CameraSpriteGroup,
                  inventoryHeight: int,
                  inventoryWidth: int,
+                 center: Vector2,
+                 offset: Vector2,
                  selectedItem: Item | None = None):
 
         super().__init__()
         self._inventoryHeight = inventoryHeight
         self._inventoryWidth = inventoryWidth
         self._selectedItem = selectedItem
-        self._inventoryList: list[Slot] = [Slot()] * inventoryWidth * inventoryHeight
+        self._inventoryList: list[Slot] = [Slot(Vector2(x * 32, y * 32) + center - Vector2(inventoryWidth * 16, inventoryHeight * 16) + offset)
+                                           for x in range(0, inventoryWidth)
+                                           for y in range(0, inventoryHeight)]
         self._isOpened: bool = False
 
         self._visibleSprites = visibleSprites
-        self._image: Surface = pygame.image.load("path_to_image")
-        self._rect = self.image.get_rect()
-        self._center = pygame.math.Vector2()
+        self.image: Surface = pygame.Surface([inventoryWidth * 32, inventoryHeight * 32])
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
     @property
     def selectedItem(self) -> Item | None:
@@ -46,16 +51,18 @@ class Inventory(pygame.sprite.Sprite):
     def addItem(self, item: Item) -> None:
         first: Slot | None = next(filter(lambda slot: (slot.isEmpty()), self._inventoryList), None)
         if first is None and self._selectedItem is not None:
-            pos = pygame.mouse.get_pos()
-            self._selectedItem.drop(*pos)
+            self._selectedItem.drop(self.rect.center)
             self._selectedItem = item
             return
         if first is None and self._selectedItem is None:
             self._selectedItem = item
             return
         indexOfSlot: int = self._inventoryList.index(first)
-        self._inventoryList[indexOfSlot].item = item
+        self._inventoryList[indexOfSlot].addItem(item)
         return
+
+    def conditionalUpdate(self, slot: Slot):
+        pass
 
     def update(self) -> None:
         pos = pygame.mouse.get_pos()
@@ -64,5 +71,7 @@ class Inventory(pygame.sprite.Sprite):
             return
         hooveredSlots = [s for s in self._inventoryList if s.rect.collidepoint(pos)]
         for slot in hooveredSlots:
-            slot.conditionalUpdate()
+            self.conditionalUpdate(slot)
         return
+
+

b8dd0211195a29f07e318be3d2caaa099a67615a
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-03 12:35:50 +0100

    refactoring

--- game/Inventory.py
+++ game/Inventory.py
@@ -1,5 +1,4 @@
-import pygame
-
+from config import *
 from game.Item import Item
 from game.Slot import Slot
 from game.CameraSpriteGroup import CameraSpriteGroup
@@ -10,21 +9,24 @@ class Inventory(pygame.sprite.Sprite):
                  visibleSprites: CameraSpriteGroup,
                  inventoryHeight: int,
                  inventoryWidth: int,
-                 playerCenter: tuple[int, int],):
+                 playerPos: tuple[int, int],):
 
         super().__init__()
         self._inventoryHeight = inventoryHeight
         self._inventoryWidth = inventoryWidth
         self._selectedItem = None
         self._isOpened: bool = False
-        self._playerCenter = playerCenter
-        self._offset = (-1280 // 2 + 16 * inventoryWidth + 16, -720 // 2 + 16 * inventoryHeight + 16)
+        self._playerPos = playerPos
+        self._slotRec = pygame.image.load("./graphics/ui/slot.png").get_size()
+        self._offset = ((- WINDOW_WIDTH + self._slotRec[0] * (inventoryWidth + 1)) // 2,
+                        (- WINDOW_HEIGTH + self._slotRec[1] * (inventoryHeight + 1)) // 2)
         self._inventoryList: list[Slot] = [Slot(self.__calculateSlotPosition((x, y)))
                                            for x in range(0, inventoryWidth)
                                            for y in range(0, inventoryHeight)]
 
         self._visibleSprites = visibleSprites
-        self.image = pygame.Surface([inventoryWidth * 32, inventoryHeight * 32], pygame.SRCALPHA, 32).convert_alpha()
+        self.image = pygame.Surface([inventoryWidth * self._slotRec[0], inventoryHeight * self._slotRec[1]],
+                                    pygame.SRCALPHA, 32).convert_alpha()
         self.rect = self.image.get_rect()
 
     @property
@@ -36,15 +38,15 @@ class Inventory(pygame.sprite.Sprite):
         return self._isOpened
 
     @property
-    def playerCenter(self) -> tuple[int, int]:
-        return self._playerCenter
+    def playerPos(self) -> tuple[int, int]:
+        return self._playerPos
 
-    @playerCenter.setter
-    def playerCenter(self, playerCenter: tuple[int, int]) -> None:
-        self._playerCenter = playerCenter
+    @playerPos.setter
+    def playerPos(self, playerCenter: tuple[int, int]) -> None:
+        self._playerPos = playerCenter
 
-        self.rect.center = [self._playerCenter[1] + self._offset[1] - 16,
-                            self._playerCenter[1] + self._offset[1] - 16]
+        self.rect.center = [self._playerPos[1] + self._offset[1] - self._slotRec[0],
+                            self._playerPos[1] + self._offset[1] - self._slotRec[1]]
 
         for i, slot in enumerate(self._inventoryList):
             y = i // self._inventoryWidth
@@ -52,8 +54,9 @@ class Inventory(pygame.sprite.Sprite):
             slot.rect.center = self.__calculateSlotPosition((x, y))
 
     def __calculateSlotPosition(self, index: tuple[int, int]) -> tuple[int, int]:
-        return (index[0] * 32 + self._playerCenter[0] - self._inventoryWidth * 16 + self._offset[0],
-                index[1] * 32 + self._playerCenter[1] - self._inventoryHeight * 16 + self._offset[1])
+
+        return ((index[0] + self._inventoryWidth // 2) * self._slotRec[0] + self._offset[0] + self._playerPos[0],
+                (index[1] + self._inventoryHeight // 2) * self._slotRec[1] + self._offset[1] + self._playerPos[1])
 
     def handleOpening(self):
         if self._isOpened:

530c39c958845b962291505b38f549d312f68f30
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-03 02:15:58 +0100

    added inventory

--- game/Player.py
+++ game/Player.py
@@ -1,9 +1,14 @@
 import pygame
+from pygame.math import Vector2
+
 from game.Entity import Entity
+from game.Inventory import Inventory
+
 
 class Player(Entity):
     def __init__(self, groups, obstacleSprites, playerData):
         super().__init__(groups, obstacleSprites, playerData)
+        self.inventory = Inventory(groups[0], 5, 10, self.rect.center)
 
 
     def input(self):
@@ -20,8 +25,14 @@ class Player(Entity):
             self.direction.x = -1
         elif pressedKeys[pygame.K_d]:
             self.direction.x = 1
-            
+
+        # inventory
+        if pressedKeys[pygame.K_e]:
+            self.inventory.handleOpening()
+
 
     def update(self):
         self.input()
         self.move()
+        if self.inventory.isOpened:
+            self.inventory.playerCenter = self.rect.center

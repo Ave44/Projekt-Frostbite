f4eb17c23121b427b4f000f4b20815d756316973
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-05 22:39:51 +0100

    typo

--- game/CameraSpriteGroup.py
+++ game/CameraSpriteGroup.py
@@ -1,11 +1,11 @@
 import pygame
-from config import WINDOW_HEIGTH, WINDOW_WIDTH
+from config import WINDOW_HEIGHT, WINDOW_WIDTH
 
 class CameraSpriteGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.displaySurface = pygame.display.get_surface()
-        self.halfWindowHeight = WINDOW_HEIGTH // 2
+        self.halfWindowHeight = WINDOW_HEIGHT // 2
         self.halfWindowWidth = WINDOW_WIDTH // 2
         self.offset = pygame.math.Vector2()
 

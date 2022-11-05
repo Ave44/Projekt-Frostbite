f3905a027918ea2c1547f7f6f26d20ab934739d4
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-05 22:42:33 +0100

    item logic

--- game/Item.py
+++ game/Item.py
@@ -1,12 +1,29 @@
+from abc import ABC
+
 import pygame.sprite
+import shortuuid
 
-class Item(pygame.sprite.Sprite):
-    def __init__(self):
+class Item(pygame.sprite.Sprite, ABC):
+    def __init__(self, name: str, pathToImage: str, pathToIcon: str):
         super().__init__()
-        self.rect = None
+        self._id = shortuuid.uuid()
+        self._name = name
 
-    def drop(self, param):
-        pass
+        self.image = pygame.image.load(pathToImage)
+        self.icon = pygame.image.load(pathToIcon)
+        self.rect = self.image.get_rect()
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def drop(self, pos: tuple[int, int]) -> None:
+        self.remove()
+        self.rect.center = pos
 
     def use(self):
         pass

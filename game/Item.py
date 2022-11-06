bfa63106cccf2536f1fe9fc00d189d73f888dea1
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-06 01:05:53 +0100

    add default item icon and image

--- game/Item.py
+++ game/Item.py
@@ -1,21 +1,46 @@
+import os.path
 from abc import ABC
 
 import pygame.sprite
 import shortuuid
+from pygame import Surface
+
+from config import ROOT_PATH
+
 
 class Item(pygame.sprite.Sprite, ABC):
-    def __init__(self, name: str, pathToImage: str, pathToIcon: str):
+    def __init__(self, name: str, pathToImage: str = None, pathToIcon: str = None):
         super().__init__()
         self._id = shortuuid.uuid()
         self._name = name
 
-        self.image = pygame.image.load(pathToImage)
-        self.icon = pygame.image.load(pathToIcon)
+        self._image = pygame.image.load(pathToImage) if pathToImage is not None \
+            else pygame.image.load(os.path.join(ROOT_PATH, "graphics", "items", "undefined.png"))
+        self._icon = pygame.image.load(pathToIcon) if pathToIcon is not None \
+            else pygame.image.load(os.path.join(ROOT_PATH, "graphics", "items", "undefined.png"))
         self.rect = self.image.get_rect()
 
     @property
     def id(self) -> str:
         return self._id
+
+    @property
+    def image(self) -> Surface:
+        return self._image
+
+    @image.setter
+    def image(self, image: Surface):
+        self.image = image
+        self.rect = image.get_rect()
+        return
+
+    @property
+    def icon(self):
+        return self.icon
+
+    @icon.setter
+    def icon(self, icon: Surface):
+        self.icon = icon
 
     @property
     def name(self) -> str:

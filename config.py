0f85915eb8f05ccf813f9b11835fdb58760dc528
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-06 01:03:45 +0100

    refactoring

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os.path
+
 import pygame
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 FPS = 60
-TILESIZE = 64
+TILE_SIZE = 64
 CAPTION = 'Project Frostbite'
-ICON = pygame.image.load('./graphics/icon.png')
+ICON = pygame.image.load(os.path.join(ROOT_PATH, "graphics", "icon.png"))

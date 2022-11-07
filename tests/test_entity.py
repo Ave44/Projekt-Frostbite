be8787759dce32d5bce38d061b79309e13703e52
Paweł Olszewski <monte.paw@gmail.com>
2022-11-07 20:52:37 +0100

    Quick tests refactor

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -2,9 +2,16 @@ import unittest
 from game.Entity import Entity
 
 class entityTests(unittest.TestCase):
+    entityData = {
+        "position_center": [0,0],
+        "path_to_image_up": "./graphics/icon.png",
+        "path_to_image_down": "./graphics/icon.png",
+        "path_to_image_left": "./graphics/icon.png",
+        "path_to_image_right": "./graphics/icon.png"
+    }
 
     def test_entity_movement_diagonal_U_R(self):
-        entity = Entity([],[])
+        entity = Entity([],[], self.entityData)
         entity.speed = 10
         entity.direction.x = 1
         entity.direction.y = 1
@@ -12,7 +19,7 @@ class entityTests(unittest.TestCase):
         self.assertEqual(entity.rect.topleft, [7,7], "Movement direction vector should be normalized")
     
     def test_entity_movement_diagonal_U_L(self):
-        entity = Entity([],[])
+        entity = Entity([],[], self.entityData)
         entity.speed = 10
         entity.direction.x = -1
         entity.direction.y = 1
@@ -20,7 +27,7 @@ class entityTests(unittest.TestCase):
         self.assertEqual(entity.rect.topleft, [-7,7], "Movement direction vector should be normalized")
 
     def test_entity_movement_diagonal_D_R(self):
-        entity = Entity([],[])
+        entity = Entity([],[], self.entityData)
         entity.speed = 10
         entity.direction.x = 1
         entity.direction.y = -1
@@ -28,7 +35,7 @@ class entityTests(unittest.TestCase):
         self.assertEqual(entity.rect.topleft, [7,-7], "Movement direction vector should be normalized")
 
     def test_entity_movement_diagonal_D_L(self):
-        entity = Entity([],[])
+        entity = Entity([],[], self.entityData)
         entity.speed = 10
         entity.direction.x = -1
         entity.direction.y = -1

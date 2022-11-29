3fd2d8b6afbd914d9e9d2e2e580f9e06fed3df58
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-29 19:51:37 +0100

    added die logic for player

--- game/Player.py
+++ game/Player.py
@@ -1,22 +1,27 @@
 import pygame
+from pygame.math import Vector2
 
 from game.Entity import Entity
+from game.UiSpriteGroup import UiSpriteGroup
 from game.items.Item import Item
+from game.ui.Bar import Bar
 from game.ui.SelectedItem import SelectedItem
 from game.ui.inventory.Inventory import Inventory
 
 
 class Player(Entity):
-    def __init__(self, 
+    def __init__(self,
                 groups: list[pygame.sprite.Group],
                 obstacleSprites: pygame.sprite.Group,
                 playerData,
-                inventory: Inventory):
+                inventory: Inventory,
+                maxHealth: int
+                 ):
 
-        super().__init__(groups, obstacleSprites, playerData)
+        super().__init__(groups, obstacleSprites, playerData, maxHealth, maxHealth)
         self.selectedItem = SelectedItem(self)
         self.inventory = inventory
-
+        self.healthBar = Bar(Vector2(10, 40), self.maxHealth, self.currentHealth, 20, 200, 'red')
 
     def moveUp(self):
         self.direction.y = -1
@@ -36,3 +41,8 @@ class Player(Entity):
 
     def update(self):
         self.move()
+        self.healthBar.update(self.currentHealth)
+
+    def die(self):
+        self.healthBar.update(self.currentHealth)
+        self.remove(*self.groups())

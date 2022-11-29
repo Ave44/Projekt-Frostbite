3590222252c1b13e29bdc2000a1ed144ebbabbac
Kacper Kruger <kacper.kruger1@gmail.com>
2022-11-29 20:01:29 +0100

    added ui bar config values

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os.path
 import pygame
 
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 FPS = 60
@@ -10,3 +11,7 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 ICON = pygame.image.load(f"{ROOT_PATH}/graphics/icon.png")
 SLOTSIZE = pygame.image.load(f"{ROOT_PATH}/graphics/ui/slot.png").get_size()[0]
 SLOTGAP = 6
+
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = 'white'
+UI_BORDER_SIZE = 3

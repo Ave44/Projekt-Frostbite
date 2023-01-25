8ffdbe80146d6dbece592c5cdd350e53008fe8ef
Paweł Olszewski <monte.paw@gmail.com>
2023-01-25 22:17:59 +0100

    Various code improvements, and refactors

--- gameInitialization/GenerateMap.py
+++ gameInitialization/GenerateMap.py
@@ -5,7 +5,7 @@ import random
 
 biomesId = {0: 'sea', 1: 'grassland'}
 
-def generateGroundAndWater(mapSize, seed=random.randint(1, 1000)):
+def generateIdMatrix(mapSize: int, seed=random.randint(1, 1000)):
     octaves = 10
     noise = PerlinNoise(octaves, seed)
     pic = [[noise([i / mapSize, j / mapSize]) for j in range(mapSize)] for i in range(mapSize)]
@@ -13,83 +13,80 @@ def generateGroundAndWater(mapSize, seed=random.randint(1, 1000)):
     if (mapSize < 5):
         mapSize = 5
 
-    mapAreaArray = [[0 for x in range(mapSize)] for y in range(mapSize)]
+    idMatrix = [[0 for x in range(mapSize)] for y in range(mapSize)]
 
     r = (mapSize - 2) // 2 + 1
     for y in range(mapSize):
         for x in range(mapSize):
             if abs((x - (mapSize // 2)) ** 2 + (y - (mapSize // 2)) ** 2) < r ** 2 and pic[x][y] < 0.2:
-                mapAreaArray[x][y] = 1
+                idMatrix[x][y] = 1
             else:
-                mapAreaArray[x][y] = 0
+                idMatrix[x][y] = 0
 
-    return mapAreaArray
+    return idMatrix
 
-def replaceGroundWithBeach(mapAreaArray):
-    tilesMatrix = [[biomesId[mapAreaArray[row][column]] for column in range(len(mapAreaArray))] for row in range(len(mapAreaArray))]
+def replaceIdWithNames(idMatrix):
+    namesMatrix = [[biomesId[idMatrix[row][column]] for column in range(len(idMatrix))] for row in range(len(idMatrix))]
 
-    for y in range(1, len(mapAreaArray) - 1):
-        for x in range(1, len(mapAreaArray) - 1):
-            if mapAreaArray[x][y] != 0:
-                if mapAreaArray[x][y - 1] == 0:
-                    tilesMatrix[x][y] = tilesMatrix[x][y] + "L"
-                if mapAreaArray[x][y + 1] == 0:
-                    tilesMatrix[x][y] = tilesMatrix[x][y] + "R"
-                if mapAreaArray[x - 1][y] == 0:
-                    tilesMatrix[x][y] = tilesMatrix[x][y] + "T"
-                if mapAreaArray[x + 1][y] == 0:
-                    tilesMatrix[x][y] = tilesMatrix[x][y] + "B"
+    for y in range(1, len(idMatrix) - 1):
+        for x in range(1, len(idMatrix) - 1):
+            if idMatrix[x][y] != 0:
+                checkForBorder(idMatrix, namesMatrix, x, y,  0, -1, "L")
+                checkForBorder(idMatrix, namesMatrix, x, y,  0,  1, "R")
+                checkForBorder(idMatrix, namesMatrix, x, y, -1,  0, "T")
+                checkForBorder(idMatrix, namesMatrix, x, y,  1,  0, "B")
 
-    return tilesMatrix
+    return namesMatrix
+
+def checkForBorder(idMatrix, namesMatrix, x: int, y: int, xOffset: int, yOffset: int, borderTag: str):
+    if idMatrix[x + xOffset][y + yOffset] == 0:
+        namesMatrix[x][y] = namesMatrix[x][y] + borderTag
 
 # returns {name: {image: pygame.image, walkable: bool}} loading all images in advance makes the code faster
 def loadTilesData():
     tilesData = {}
     tileId = 0
-    for biom in os.listdir("./graphics/tiles/walkable"):
-        for imageFile in os.listdir(f"./graphics/tiles/walkable/{biom}"):
-            name = imageFile[:-4]
-            image = pygame.image.load(f"./graphics/tiles/walkable/{biom}/{imageFile}")
-            tilesData[name] = {"image": image, "walkable": True}
-        tileId += 1
-            
-    for biom in os.listdir("./graphics/tiles/collidable"):
-        for imageFile in os.listdir(f"./graphics/tiles/collidable/{biom}"):
-            name = imageFile[:-4]
-            image = pygame.image.load(f"./graphics/tiles/collidable/{biom}/{imageFile}")
-            tilesData[name] = {"image": image, "walkable": False}
-        tileId += 1
+    loadTilesByType(tilesData, tileId, "walkable", True)
+    loadTilesByType(tilesData, tileId, "collidable", False)
 
     return tilesData
 
-def a(namesMap):
+def loadTilesByType(tilesData: dict, tileId: int, subfolderName: str, isWalkable: bool):
+    for biom in os.listdir(f"./graphics/tiles/{subfolderName}"):
+        for imageFile in os.listdir(f"./graphics/tiles/{subfolderName}/{biom}"):
+            name = imageFile[:-4]
+            image = pygame.image.load(f"./graphics/tiles/{subfolderName}/{biom}/{imageFile}")
+            tilesData[name] = {"image": image, "walkable": isWalkable}
+        tileId += 1
+
+def populateNameMatrixWithData(namesMatrix):
     tilesData = loadTilesData()
-    mapSize = len(namesMap)
+    mapSize = len(namesMatrix)
     dataMap = [[None for x in range(mapSize)] for y in range(mapSize)]
 
     for y in range(mapSize):
         for x in range(mapSize):
-            dataMap[y][x] = tilesData[namesMap[y][x]]
+            dataMap[y][x] = tilesData[namesMatrix[y][x]]
     
     return dataMap
 
 
-def generateMap(mapSize):
-    idMap = generateGroundAndWater(mapSize)
-    # for i in idMap:
+def generateMap(mapSize: int):
+    idMatrix = generateIdMatrix(mapSize)
+    # for i in idMatrix:
     #     print(i)
 
-    namesMap = replaceGroundWithBeach(idMap)
-    # for i in namesMap:
+    namesMatrix = replaceIdWithNames(idMatrix)
+    # for i in namesMatrix:
     #     print(i)
 
-    dataMap = a(namesMap)
-    # for i in dataMap:
+    dataMatrix = populateNameMatrixWithData(namesMatrix)
+    # for i in dataMatrix:
     #     print(i)
 
-    return dataMap
+    return dataMatrix
 
-# map = generateGroundAndWater(64)
+# map = generateIdMatrix(64)
 # a = replaceGroundWithBeach(1, map)
 # for i in map:
 #     for l in i:

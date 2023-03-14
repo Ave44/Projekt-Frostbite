b1d069a9903dc99a7d8c4ce824c3e92b678720fe
Kacper Kruger <kacper.kruger1@gmail.com>
2023-03-14 13:44:59 +0100

    updated tree, now has lifespan

--- game/objects/Tree.py
+++ game/objects/Tree.py
@@ -10,18 +10,26 @@ from game.objects.Flammable import Flammable
 
 
 class Tree(Flammable):
+    _LIFESPAN = 10000
+
     def __init__(self, visibleGroup: Group, obstaclesGroup: Group, bottomCenter: Vector2(),
                  clock: Clock):
         image = pygame.image.load(f"{ROOT_PATH}/graphics/objects/tree/tree.png")
         super().__init__(visibleGroup, obstaclesGroup,
                          bottomCenter, 10, ToolType.AXE, image, clock)
-
-    def localUpdate(self):
-        pass
+        self.age = 0
 
     def interact(self) -> None:
-        # do something
-        pass
+        print("interacted with medium tree")  # in the future there will be a real implementation
 
     def dropItem(self) -> None:
-        Sword(self.visibleGroup, self.rect.center)
+        Sword(self.visibleGroup, self.rect.center)  # in the future there will be a real implementation
+
+    def localUpdate(self):
+        if self.isOnFire:
+            return
+        self.age += self.clock.get_time()
+        if self.age >= self._LIFESPAN:
+            self.visibleGroup.remove(self)
+            self.obstaclesGroup.remove(self)
+            Snag(self.visibleGroup, self.obstaclesGroup, self.rect.midbottom, self.clock)

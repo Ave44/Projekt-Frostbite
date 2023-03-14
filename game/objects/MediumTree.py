3d1f6d2f0d6c5eb4f6191a100b53cf4086e97f9e
Kacper Kruger <kacper.kruger1@gmail.com>
2023-03-14 13:33:27 +0100

    added medium tree

--- game/objects/MediumTree.py
+++ game/objects/MediumTree.py
@@ -7,19 +7,19 @@ from config import ROOT_PATH
 from game.items.Sword import Sword
 from game.items.ToolType import ToolType
 from game.objects.Flammable import Flammable
-from game.objects.MediumTree import MediumTree
+from game.objects.Tree import Tree
 
 
-class SmallTree(Flammable):
-    _LIFESPAN = 20000
+class MediumTree(Flammable):
+    _LIFESPAN = 10000
 
     def __init__(self, visibleGroup: Group, obstaclesGroup: Group, midBottom: Vector2, clock: Clock):
-        image = pygame.image.load(f"{ROOT_PATH}/graphics/objects/smallTree.png")
-        super().__init__(visibleGroup, obstaclesGroup, midBottom, 5, ToolType.AXE, image, clock)
+        image = pygame.image.load(f"{ROOT_PATH}/graphics/objects/mediumTree.png")
+        super().__init__(visibleGroup, obstaclesGroup, midBottom, 10, ToolType.AXE, image, clock)
         self.age = 0
 
     def interact(self) -> None:
-        print("interacted with small tree")  # in the future there will be a real implementation
+        print("interacted with medium tree")  # in the future there will be a real implementation
 
     def dropItem(self) -> None:
         Sword(self.visibleGroup, self.rect.center)  # in the future there will be a real implementation
@@ -31,4 +31,4 @@ class SmallTree(Flammable):
         if self.age >= self._LIFESPAN:
             self.visibleGroup.remove(self)
             self.obstaclesGroup.remove(self)
-            MediumTree(self.visibleGroup, self.obstaclesGroup, self.rect.midbottom, self.clock)
+            Tree(self.visibleGroup, self.obstaclesGroup, self.rect.midbottom, self.clock)

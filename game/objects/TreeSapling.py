fa005dd47bee646edfdfb3b2e2e32bd65936ba3d
Kacper Kruger <kacper.kruger1@gmail.com>
2023-03-14 12:46:37 +0100

    added sapling

---
+++ game/objects/TreeSapling.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame import Vector2
+from pygame.sprite import Group
+from pygame.time import Clock
+
+from config import ROOT_PATH
+from game.items.Sword import Sword
+from game.items.ToolType import ToolType
+from game.objects.Object import Object
+from game.objects.Tree import Tree
+
+
+class TreeSapling(Object):
+    _LIFESPAN = 36000
+
+    def __init__(self, visibleGroup: Group, obstaclesGroup: Group, midBottom: Vector2, clock: Clock):
+        image = pygame.image.load(f"{ROOT_PATH}/graphics/objects/sapling.png")
+        super().__init__(visibleGroup, obstaclesGroup, midBottom, 1, ToolType.HAND, image)
+        self.clock = clock
+        self.age = 0
+
+    def interact(self) -> None:
+        print("interacted with sapling")
+
+    def dropItem(self) -> None:
+        Sword(self.visibleGroup, self.rect.center)
+
+    def update(self) -> None:
+        self.age += self.clock.get_time()
+        if self.age >= self._LIFESPAN:
+            self.visibleGroup.remove(self)
+            self.obstaclesGroup.remove(self)
+            Tree(self.visibleGroup, self.obstaclesGroup, self.rect.midbottom, self.clock)

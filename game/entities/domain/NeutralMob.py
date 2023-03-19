377ea117eab40f09baf8e131fdfc69063e37c862
Kacper Kruger <kacper.kruger1@gmail.com>
2023-03-19 23:06:08 +0100

    added neutral mob interface

---
+++ game/entities/domain/NeutralMob.py
@@ -0,0 +1,23 @@
+from abc import ABC
+
+from game.entities.domain.EnemyMob import EnemyMob
+from game.entities.domain.State import State
+
+
+class NeutralMob(EnemyMob, ABC):
+    def localUpdate(self):
+        if not self.target and self.state == State.DAMAGED:
+            closestEntity = self.findClosestOtherEntity()
+            if closestEntity and self.isInSightRange(closestEntity):
+                self.target = closestEntity
+                self.moveToOrAttack(self.target)
+                return
+            self.moveRandomly()
+            return
+
+        if not self.isInSightRange(self.target):
+            self.target = None
+            self.destinationPosition = None
+            return
+
+        self.moveToOrAttack(self.target)

d7a19d25793a34a4d93bd23f819bf31469c23691
Kacper Kruger <kacper.kruger1@gmail.com>
2023-03-20 15:00:08 +0100

    added mock of rabbit hole

--- game/objects/RabbitHole.py
+++ game/objects/RabbitHole.py
@@ -1,29 +1,54 @@
 import pygame
 from pygame import Vector2
-from pygame.sprite import Group
 
 from config import ROOT_PATH
 from game.items.ToolType import ToolType
 from game.objects.domain.Object import Object
+from game.spriteGroups.CameraSpriteGroup import CameraSpriteGroup
 
 
 class RabbitHole(Object):
-    def __init__(self, visibleGroup: Group, midBottom: Vector2):
+    def __init__(self, visibleGroup: CameraSpriteGroup, midBottom: Vector2):
         image = pygame.image.load(f"{ROOT_PATH}/graphics/objects/rabbit_hole.png")
         super().__init__(visibleGroup, midBottom, 50, ToolType.SHOVEL, image)
         self.connectedRabbit = []
+        self.hiddenRabbits = []
+        self.daysFromConnectedRabbitChange = 0
 
     def hideRabbit(self, rabbit: Rabbit):
-        pass
+        if len(self.hiddenRabbits) >= 3:
+            ValueError("The hole is full")
+        self.hiddenRabbits.append(rabbit)
+        rabbit.changeStatus(Status.HIDDEN)
 
     def releaseRabbits(self):
-        pass
+        for rabbit in self.hiddenRabbits:
+            rabbit.changeStatus(Status.NORMAL)
 
     def spawnRabbit(self):
-        pass
+        newRabbit = Rabbit(self.visibleGroup, self.rect.center, self)
+        self.connectedRabbit.append(newRabbit)
+        self.daysFromConnectedRabbitChange = 0
+
+    def disconnectRabbit(self, rabbit: Rabbit):
+        if rabbit in self.connectedRabbit:
+            self.connectedRabbit.remove(rabbit)
+            self.daysFromConnectedRabbitChange = 0
+        else:
+            ValueError("Rabbit not found")
 
     def drop(self) -> None:
-        pass
+        if not self.hiddenRabbits:
+            self.spawnRabbit()
+        else:
+            self.releaseRabbits()
 
     def interact(self) -> None:
-        pass
+        print("interacted with rabbit hole")
+
+    def handleDayChange(self):
+        self.daysFromConnectedRabbitChange += 1
+
+    def update(self):
+        if len(self.connectedRabbit) < 3 and self.daysFromConnectedRabbitChange >= 10:
+            self.spawnRabbit()

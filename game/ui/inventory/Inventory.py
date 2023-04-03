c17fc0c834581778739b4b4e85fbf9bf9abacec8
Paweł Olszewski <monte.paw@gmail.com>
2023-04-03 20:59:28 +0200

    Quick import fix

--- game/ui/inventory/Inventory.py
+++ game/ui/inventory/Inventory.py
@@ -1,4 +1,4 @@
-from pygame import Surface
+from pygame import Surface, SRCALPHA
 from pygame.math import Vector2
 from pygame.sprite import Sprite, Group
 from constants import BG_COLOR, SLOT_GAP, SLOT_SIZE
@@ -21,7 +21,7 @@ class Inventory(Sprite):
         self.spriteGroup = spriteGroup
         self.image = Surface(
             [inventoryWidth * (SLOT_SIZE + SLOT_GAP) + SLOT_GAP, inventoryHeight * (SLOT_SIZE + SLOT_GAP) + SLOT_GAP],
-            pygame.SRCALPHA, 32)
+            SRCALPHA, 32)
         self.image.fill(BG_COLOR)
         self.rect = self.image.get_rect()
         self.rect.center = center

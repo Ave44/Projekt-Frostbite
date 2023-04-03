f92ef605478260b2be8229d7a370e847eebbcf38
MackowiakMateusz <Darkstaron123@gmail.com>
2023-04-03 20:37:24 +0200

    Goblin Hideout Refaktor

--- game/objects/GoblinHideout.py
+++ game/objects/GoblinHideout.py
@@ -15,7 +15,7 @@ class GoblinHideout(Object):
     def __init__(self, visibleGroup: CameraSpriteGroup, obstacleSprites: ObstacleSprites,
                  loadedImages: LoadedImages, midBottom: Vector2, clock: Clock):
         image = loadedImages.goblinHideout
-        super().__init__(visibleGroup, midBottom, 50, ToolType.PICKAXE, image)
+        Object.__init__(self,visibleGroup, midBottom, 50, ToolType.PICKAXE, image)
         self.loadedImages = loadedImages
         self.goblins = []
         self.daysFromGoblinsChange = 0

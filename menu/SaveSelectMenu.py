98b6d34769bb2d295fe910df8452f539f3c32a6a
Paweł Olszewski <monte.paw@gmail.com>
2023-05-15 00:46:50 +0200

    added option of removing savefiles from savefile selection view

--- menu/SaveSelectMenu.py
+++ menu/SaveSelectMenu.py
@@ -1,7 +1,9 @@
 import json
+from os import remove
 from os.path import isfile
+from pygame import Vector2
 
-from constants import FONT_MENU_COLOR
+from constants import FONT_MENU_COLOR, DELETE_SAVEFILE_COLOR
 from Config import Config
 from game.Game import Game
 from menu.Menu import Menu
@@ -27,29 +29,48 @@ class SaveSelectMenu(Menu):
         else:
             self.createGameMenu.initiateCreateGame()
 
-        
+    def deleteSavefile(self, savefileName: str):
+        remove(f"./savefiles/{savefileName}.json")
+        self.initiateView()
+
+    def createDeleteSaveButton(self, buttonsList: list, savefileName: str, position: Vector2) -> None:
+        if isfile(f"./savefiles/{savefileName}.json"):
+            buttonsList.append(Button(pos=position,
+                                      textInput="X",
+                                      font=self.config.fontBig,
+                                      action=self.deleteSavefile,
+                                      actionArgument=savefileName,
+                                      hoveringColor=DELETE_SAVEFILE_COLOR))
+            
+    def createSavefileButton(self, buttonsList: list, buttonText: str, savefileName: str, position: Vector2) -> Button:
+        button = Button(pos=position,
+                        textInput=buttonText,
+                        font=self.config.fontBig,
+                        action=self.loadSavefile,
+                        actionArgument=savefileName)
+        buttonsList.append(button)
+        return button
 
     def createButtons(self) -> list[Button]:
-        playButton = Button(pos=(0.5 * self.config.WINDOW_WIDTH, 0.347 * self.config.WINDOW_HEIGHT),
-                            textInput="SAVEFILE 1",
-                            font=self.config.fontBig,
-                            action=self.loadSavefile,
-                            actionArgument="savefile1")
-        createGameButton = Button(pos=(0.5 * self.config.WINDOW_WIDTH, 0.486 * self.config.WINDOW_HEIGHT),
-                                  textInput="SAVEFILE 2",
-                                  font=self.config.fontBig,
-                                  action=self.loadSavefile,
-                            actionArgument="savefile2")
-        optionsButton = Button(pos=(0.5 * self.config.WINDOW_WIDTH, 0.625 * self.config.WINDOW_HEIGHT),
-                               textInput="SAVEFILE 3",
-                               font=self.config.fontBig,
-                               action=self.loadSavefile,
-                            actionArgument="savefile3")
-        quitButton = Button(pos=(0.5 * self.config.WINDOW_WIDTH, 0.764 * self.config.WINDOW_HEIGHT),
+        buttonsList = []
+
+        savefile1Button = self.createSavefileButton(buttonsList, "SAVEFILE 1", "savefile1", Vector2(0.5 * self.config.WINDOW_WIDTH, 0.347 * self.config.WINDOW_HEIGHT))
+        savefile2Button = self.createSavefileButton(buttonsList, "SAVEFILE 2", "savefile2", Vector2(0.5 * self.config.WINDOW_WIDTH, 0.486 * self.config.WINDOW_HEIGHT))
+        savefile3Button = self.createSavefileButton(buttonsList, "SAVEFILE 3", "savefile3", Vector2(0.5 * self.config.WINDOW_WIDTH, 0.625 * self.config.WINDOW_HEIGHT))
+
+        deleteButton1Position = Vector2(savefile1Button.rect.right + 100, 0.347 * self.config.WINDOW_HEIGHT)
+        deleteButton2Position = Vector2(savefile1Button.rect.right + 100, 0.486 * self.config.WINDOW_HEIGHT)
+        deleteButton3Position = Vector2(savefile1Button.rect.right + 100, 0.625 * self.config.WINDOW_HEIGHT)
+        self.createDeleteSaveButton(buttonsList, "savefile1", deleteButton1Position)
+        self.createDeleteSaveButton(buttonsList, "savefile2", deleteButton2Position)
+        self.createDeleteSaveButton(buttonsList, "savefile3", deleteButton3Position)
+
+        backButton = Button(pos=(0.5 * self.config.WINDOW_WIDTH, 0.764 * self.config.WINDOW_HEIGHT),
                             textInput="BACK",
                             font=self.config.fontBig,
                             action=self.backAction)
-        return [playButton, optionsButton, quitButton, createGameButton]
+        buttonsList.append(backButton)
+        return buttonsList
 
 
     def initiateView(self) -> None:
